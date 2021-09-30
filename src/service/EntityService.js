434c31f0bcfc4fa2070896968f38d583d1f22eb6
yukina <1p41p4jejo@gmail.com>
2021-10-01 03:48:52 +0800

    Add getEntitySource to entity service

--- src/service/EntityService.js
+++ src/service/EntityService.js
@@ -1,5 +1,7 @@
 //@ts-check
 import AbstractService from './AbstractService';
+import EntitySource from './entity/EntitySource';
+import KeyValueEntitySource from './entity/KeyValueEntitySource';
 
 export default class extends AbstractService {
 
@@ -7,4 +9,26 @@ export default class extends AbstractService {
         return "EntityService";
     }
 
+    /**
+     * getEntitySource
+     * @param {string} spreadsheetId 
+     * @param {string} sheetName 
+     * @returns {EntitySource}
+     */
+    getEntitySource(spreadsheetId, sheetName) {
+        let sheet = SpreadsheetApp.openById(spreadsheetId).getSheetByName(sheetName);
+        let range = sheet.getRange(1, 1, sheet.getLastRow(), sheet.getLastColumn());
+        return new EntitySource(range);
+    }
+
+    /**
+     * getEntitySource
+     * @param {string} spreadsheetId 
+     * @param {string} sheetName 
+     * @returns {KeyValueEntitySource}
+     */
+    getKeyValueEntity(spreadsheetId, sheetName, autoImport = false) {
+        let spreadsheet = SpreadsheetApp.openById(spreadsheetId);
+        return new KeyValueEntitySource(spreadsheet, sheetName, autoImport);    
+    }
 }
